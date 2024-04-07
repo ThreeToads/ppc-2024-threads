5856a4ff245a325eea39d8d5560df922040feefa
ThreeToads <ivan.kokin.465@gmail.com>
2024-04-08 00:28:01 +0300

    feat(lab1): add 4 files tests and algorithms

--- tasks/seq/kokin_marking_component_bin_image/include/ops_seq.hpp
+++ tasks/seq/kokin_marking_component_bin_image/include/ops_seq.hpp
@@ -1,17 +1,16 @@
 // Copyright 2023 Nesterov Alexander
 #pragma once
 
-#include <string>
-#include <vector>
-#include <utility>
 #include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "core/task/include/task.hpp"
 
 class TestTaskSequential : public ppc::core::Task {
  public:
-  explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_)
-      : Task(std::move(taskData_)) {}
+  explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
