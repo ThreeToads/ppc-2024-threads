d511b1a4f1e143216886c2fa38586c5470ae8b64
ThreeToads <ivan.kokin.465@gmail.com>
2024-04-08 17:29:42 +0300

    fix(lab1):

--- tasks/seq/kokin_marking_component_bin_image/func_tests/main.cpp
+++ tasks/seq/kokin_marking_component_bin_image/func_tests/main.cpp
@@ -61,7 +61,6 @@ TEST(kokin_marking_component_bin_image, test_3) {
   std::vector<uint8_t> out(ht * wh, 0);
   std::vector<uint8_t> cmpt = {0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 2, 0, 0, 0, 1, 1};
 
-
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
   taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(size.data()));
   taskDataSeq->inputs_count.emplace_back(size.size());
