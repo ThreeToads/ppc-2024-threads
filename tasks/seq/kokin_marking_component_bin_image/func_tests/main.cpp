9e30ed4c1c4fd719c38c35ac1fac71da41675325
ThreeToads <ivan.kokin.465@gmail.com>
2024-04-08 18:25:33 +0300

    fix(lab1):

--- tasks/seq/kokin_marking_component_bin_image/func_tests/main.cpp
+++ tasks/seq/kokin_marking_component_bin_image/func_tests/main.cpp
@@ -10,8 +10,8 @@ TEST(kokin_marking_component_bin_image, test_1) {
   uint32_t wh = 5;
   std::vector<uint32_t> size = {ht, wh};
   std::vector<uint8_t> in(ht * wh, 1);
-  std::vector<uint8_t> out(ht * wh, 0);
-  std::vector<uint8_t> cmpt(ht * wh, 0);
+  std::vector<uint32_t> out(ht * wh, 0);
+  std::vector<uint32_t> cmpt(ht * wh, 0);
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
   taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(size.data()));
@@ -114,9 +114,8 @@ TEST(kokin_marking_component_bin_image, test_5) {
   uint32_t wh = 5;
   std::vector<uint32_t> size = {ht, wh};
   std::vector<uint8_t> in = {0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1};
-  std::vector<uint8_t> cmpt = {1, 1, 0, 0, 2, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 3, 0, 0, 0};
-
-  std::vector<uint8_t> out(ht * wh, 0);
+  std::vector<uint32_t> out(ht * wh, 0);
+  std::vector<uint32_t> cmpt = {1, 1, 0, 0, 2, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 3, 0, 0, 0};
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
   taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(size.data()));
